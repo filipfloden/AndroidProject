18f74a24a785bdaa0cc20849dbf9c894199a0407
Filip Flodén <filipfloden@wlanguest140182.eduroam.hj.se>
2021-02-01 14:54:05 +0100

    Firebase Setup
    Added connection to firebase database.

--- app/src/main/java/se/ju/student/group16/androidproject/MainActivity.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/MainActivity.kt
@@ -2,10 +2,21 @@ package se.ju.student.group16.androidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database = FirebaseDatabase.getInstance().getReference("Users")
+
+        val myRef = database.child("Testing")
+
+        myRef.setValue("Hello, World!")
     }
 }
