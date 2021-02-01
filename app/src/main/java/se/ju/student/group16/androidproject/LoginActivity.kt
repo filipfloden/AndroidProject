869896fd367c9f94cca089aa02418171953a2dc6
Filip Flodén <filipfloden@wlanguest140182.eduroam.hj.se>
2021-02-01 15:43:24 +0100

    Event redirect

--- app/src/main/java/se/ju/student/group16/androidproject/LoginActivity.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/LoginActivity.kt
@@ -1,11 +1,21 @@
 package se.ju.student.group16.androidproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+
+        loginBtn.setOnClickListener {
+            startActivity(
+                    Intent(this, EventActivity::class.java)
+            )
+        }
     }
 }
