e205b1ee8ac5114366a83fcaac50c1346289ef62
Daniel Göthe <daniel@goethe.se>
2021-02-03 16:04:08 +0100

    tabview improvments
    tabview conected to fragments

--- app/src/main/java/se/ju/student/group16/androidproject/EventActivity.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/EventActivity.kt
@@ -2,11 +2,17 @@ package se.ju.student.group16.androidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 
 class EventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
 
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        viewPager.adapter = PageAdapter(supportFragmentManager)
     }
 }
