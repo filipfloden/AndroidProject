31603580bdd647df2cc150b2d6b2df7e263a87cb
Daniel Göthe <daniel@goethe.se>
2021-02-08 12:46:07 +0100

    fragemts fully functional

--- app/src/main/java/se/ju/student/group16/androidproject/EventActivity.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/EventActivity.kt
@@ -10,9 +10,44 @@ import com.google.android.material.tabs.TabLayout
 class EventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_event)
 
-        supportFragmentManager.beginTransaction().add(R.id.frame_layout, EventFragment())
+        val eventButton = findViewById<Button>(R.id.event_button)
+        val friendsButton = findViewById<Button>(R.id.friend_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        supportFragmentManager.beginTransaction().run{
+            add(R.id.frame_layout, EventFragment())
+            commit()
+        }
+
+        eventButton.setOnClickListener{
+            supportFragmentManager.beginTransaction().run{
+                replace(R.id.frame_layout, EventFragment())
+                commit()
+            }
+        }
+
+        friendsButton.setOnClickListener{
+            supportFragmentManager.beginTransaction().run{
+                replace(R.id.frame_layout, FriendsFragment())
+                commit()
+            }
+        }
+
+        settingsButton.setOnClickListener{
+            supportFragmentManager.beginTransaction().run{
+                replace(R.id.frame_layout, SettingsFragment())
+                commit()
+            }
+        }
+
+//        val logoutButton = findViewById<Button>(R.id.logoutBtn)
+//        logoutButton.setOnClickListener{
+//        }
+
+
 
 
     }
