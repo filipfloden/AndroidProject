b0aba0750a1869e3f7cc5acbc83baac1a51498e3
Filip Flodén <filipfloden@wlanguest13990.eduroam.hj.se>
2021-02-08 11:07:32 +0100

    Login/Signup
    A user is now able to sign up aswell as log in with his/her email and password

--- app/src/main/java/se/ju/student/group16/androidproject/LoginActivity.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/LoginActivity.kt
@@ -1,11 +1,20 @@
 package se.ju.student.group16.androidproject
 
 import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -14,9 +23,33 @@ class LoginActivity : AppCompatActivity() {
         val signupBtn = findViewById<Button>(R.id.signupBtn)
 
         loginBtn.setOnClickListener {
-            startActivity(
-                    Intent(this, EventActivity::class.java)
-            )
+
+
+            val usernameInput = findViewById<EditText>(R.id.usernameInput)
+            val passwordInput = findViewById<EditText>(R.id.passwordInput)
+            auth = FirebaseAuth.getInstance()
+            auth.signInWithEmailAndPassword(usernameInput.text.toString(), passwordInput.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("login", "signInWithEmail:success")
+                            val user = auth.currentUser
+                            Log.d("user", user.toString())
+                            //updateUI(user)
+                            val intent = Intent(this, EventActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("login", "signInWithEmail:failure", task.exception)
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show()
+                            //updateUI(null)
+                            // ...
+                        }
+
+                        // ...
+                    }
         }
 
         signupBtn.setOnClickListener {
@@ -24,5 +57,12 @@ class LoginActivity : AppCompatActivity() {
                     Intent(this, SignUpActivity::class.java)
             )
         }
+        /*
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(android.R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+         */
     }
 }
