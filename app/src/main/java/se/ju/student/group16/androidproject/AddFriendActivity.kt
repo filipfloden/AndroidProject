c5e7204120b44353150d6c4c9e9d802162e93107
Filip Flodén <33715227+filipfloden@users.noreply.github.com>
2021-03-05 17:24:29 +0100

    Friend system implemenation
    Added custom adapter for the add friend users listview with a functional search view which filters the input text string. On submit crashes which still needs to be fixed.

--- app/src/main/java/se/ju/student/group16/androidproject/AddFriendActivity.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/AddFriendActivity.kt
@@ -3,10 +3,12 @@ package se.ju.student.group16.androidproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.SearchView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -15,13 +17,14 @@ class AddFriendActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
-    private val users = mutableListOf<User>()
+    private val allUsers = mutableListOf<User>()
     private val friendRequests = mutableListOf<User>()
+    private val users = "users"
     private val received = "received"
+    private val friendsPending = "friends-pending"
     private val displayname = "displayname"
     private val email = "email"
     private val empty = ""
-    private var friends = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +35,13 @@ class AddFriendActivity : AppCompatActivity() {
 
         val usersListView = findViewById<ListView>(R.id.usersListView)
         val friendRequestListView = findViewById<ListView>(R.id.friendRequestListView)
+        val searchBar = findViewById<SearchView>(R.id.search_user)
+        searchBar.queryHint = "Search for user"
+        val friendRequestAdapter = FriendRequestAdapter(this, friendRequests)
+        val usersAdapter = UsersAdapter(this, allUsers)
         val currentUser = auth.currentUser
 
-        database.child("users").child(currentUser?.uid.toString()).child("friends-pending").get().addOnSuccessListener {
+        database.child(users).child(currentUser?.uid.toString()).child(friendsPending).get().addOnSuccessListener {
             Log.i("firebase", "Got value ${it.value}")
             for (fr in it.children){
                 if (fr.value.toString() == received){
@@ -42,29 +49,20 @@ class AddFriendActivity : AppCompatActivity() {
                 }
             }
         }.addOnFailureListener{
-            Log.e("firebase", "Error gettin data", it)
+            Log.e("firebase", "Error getting data", it)
         }
-
         database.child("users").get().addOnSuccessListener {
             Log.i("firebase", "Got value ${it.value}")
             for (user in it.children){
-                users.add(User(user.key.toString(), user.child(displayname).value.toString(), user.child("email").value.toString()))
-                if (friendRequests.contains(User(user.key.toString(), empty, empty))){
-                    friendRequests.remove(User(user.key.toString(), empty, empty))
-                    friendRequests.add(User(user.key.toString(), user.child(displayname).value.toString(), user.child(email).value.toString()))
-                    Log.i("testing string", user.key.toString() + " " + user.child(displayname).toString() + "" + user.child(email).toString())
-                }
+                if (user.key.toString() != currentUser?.uid.toString())
+                    allUsers.add(User(user.key.toString(), user.child(displayname).value.toString(), user.child("email").value.toString()))
+                    if (friendRequests.contains(User(user.key.toString(), empty, empty))){
+                        friendRequests.remove(User(user.key.toString(), empty, empty))
+                        friendRequests.add(User(user.key.toString(), user.child(displayname).value.toString(), user.child(email).value.toString()))
+                    }
             }
-            friendRequestListView.adapter = FriendRequestAdapter(
-                    this,
-                    friendRequests
-            )
-            usersListView.adapter = ArrayAdapter<User>(
-                    this,
-                    android.R.layout.simple_list_item_1,
-                    android.R.id.text1,
-                    users
-            )
+            friendRequestListView.adapter = friendRequestAdapter
+            usersListView.adapter = usersAdapter
         }.addOnFailureListener{
             Log.e("firebase", "Error getting data", it)
         }
@@ -73,8 +71,21 @@ class AddFriendActivity : AppCompatActivity() {
             val userClicked = usersListView.getItemAtPosition(position) as User
             Log.d("userclicked", userClicked.uid)
 
-            database.child("users").child(currentUser?.uid.toString()).child("friends-pending").child(userClicked.uid).setValue("sent")
-            database.child("users").child(userClicked.uid).child("friends-pending").child(currentUser?.uid.toString()).setValue("received")
+            database.child(users).child(currentUser?.uid.toString()).child(friendsPending).child(userClicked.uid).setValue("sent")
+            database.child(users).child(userClicked.uid).child(friendsPending).child(currentUser?.uid.toString()).setValue("received")
         }
+
+        searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                TODO("Not yet implemented")
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                usersAdapter.filter(newText)
+                usersAdapter.notifyDataSetChanged()
+                return false
+            }
+        })
     }
 }
---
+++ app/src/main/java/se/ju/student/group16/androidproject/UsersAdapter.kt
@@ -0,0 +1,61 @@
+package se.ju.student.group16.androidproject
+
+import android.app.Activity
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
+
+class UsersAdapter(private val context: Activity, private val user: MutableList<User>) : ArrayAdapter<User>(context, R.layout.user_row, user) {
+
+    private var userCopy = user
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private val users = "users"
+    private val friendsPending = "friends-pending"
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val inflater = context.layoutInflater
+        val rowView = inflater.inflate(R.layout.user_row, null, true)
+        val usersName = rowView.findViewById<TextView>(R.id.user_name)
+        user.sortBy { it.displayname }
+        usersName.text = user[position].displayname
+
+        val addFriend = rowView.findViewById<Button>(R.id.add_friend)
+
+        addFriend.setOnClickListener {
+            auth = FirebaseAuth.getInstance()
+            database = FirebaseDatabase.getInstance().reference
+            val currentUser = auth.currentUser
+            Log.d("Friend request", "Added " + user[position].uid)
+
+            database.child(users).child(currentUser?.uid.toString()).child(friendsPending).child(user[position].uid).setValue("sent")
+            database.child(users).child(user[position].uid).child(friendsPending).child(currentUser?.uid.toString()).setValue("received")
+        }
+        return rowView
+        //super.getView(position, convertView, parent)
+    }
+
+    fun filter(text: String?) {
+        var tempUsers = userCopy.toMutableList()
+        val text = text!!.toLowerCase()
+        user.clear()
+        if (text.isEmpty()) {
+            user.addAll(tempUsers)
+        } else {
+            for (i in tempUsers) {
+                if (i.displayname.toLowerCase().contains(text)) {
+                    user.add(i)
+                }
+            }
+        }
+        userCopy = tempUsers
+        notifyDataSetChanged()
+    }
+}
