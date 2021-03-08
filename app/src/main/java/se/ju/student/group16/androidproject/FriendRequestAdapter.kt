9689f5e0d9dcccc007c39efb4e8cbf04c7a67992
Filip Flodén <filipfloden@wlanguest138218.eduroam.hj.se>
2021-03-08 15:54:01 +0100

    Friend system implementation
    Full functionality adding friends which are displayed in the "Events" tab

--- app/src/main/java/se/ju/student/group16/androidproject/FriendRequestAdapter.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/FriendRequestAdapter.kt
@@ -7,9 +7,26 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class FriendRequestAdapter(private val context: Activity, private val user: MutableList<User>) : ArrayAdapter<User>(context, R.layout.fr_row, user) {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private val users = "users"
+    private val friends = "friends"
+    private val received = "received"
+    private val friendsPending = "friends-pending"
+    private val displayname = "displayname"
+    private val email = "email"
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+        val currentUser = auth.currentUser
 
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.fr_row, null, true)
@@ -22,12 +39,19 @@ class FriendRequestAdapter(private val context: Activity, private val user: Muta
         val denyFriend = rowView.findViewById<Button>(R.id.deny_friend)
 
         acceptFriend.setOnClickListener {
-            Log.d("FR", "Accepted " + user[position].uid)
+            database.child(users).child(currentUser?.uid.toString()).child(friends).child(user[position].uid).setValue("true").addOnSuccessListener {
+                Log.d("firebase", "Accepted " + user[position].uid)
+                database.child(users).child(user[position].uid).child(friends).child(currentUser?.uid.toString()).setValue("true")
+                database.child(users).child(user[position].uid).child(friendsPending).child(currentUser?.uid.toString()).removeValue()
+                database.child(users).child(currentUser?.uid.toString()).child(friendsPending).child(user[position].uid).removeValue()
+            }.addOnFailureListener{
+                Log.e("firebase", "Error accepting friend", it)
+            }
         }
         denyFriend.setOnClickListener {
-            Log.d("FR", "Denied " + user[position].uid)
+            database.child(users).child(user[position].uid).child(friendsPending).child(currentUser?.uid.toString()).removeValue()
+            database.child(users).child(currentUser?.uid.toString()).child(friendsPending).child(user[position].uid).removeValue()
         }
-
         return rowView
         //super.getView(position, convertView, parent)
     }
