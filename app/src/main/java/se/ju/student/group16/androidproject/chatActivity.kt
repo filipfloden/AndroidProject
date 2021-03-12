fc96f658c4e45c6cdfc7e1d152471829abe7c575
9914kach <9914kach@gmail.com>
2021-03-12 18:33:24 +0100

    chat xml

---
+++ app/src/main/java/se/ju/student/group16/androidproject/chatActivity.kt
@@ -0,0 +1,11 @@
+package se.ju.student.group16.androidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
