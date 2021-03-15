41686f4f47f3e1912be3e69890bab7e960fb08ea
Daniel Göthe <daniel@goethe.se>
2021-03-15 17:26:35 +0100

    my event activity now creates a list with all your activitys

--- app/src/main/java/se/ju/student/group16/androidproject/MyEventsActivity.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/MyEventsActivity.kt
@@ -13,11 +13,9 @@ import java.lang.reflect.Type
 
 class MyEventsActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-    private lateinit var database: DatabaseReference
+    private val database = firebaseRepository.getDatabaseReference()
     private val eventPath = "event"
     private val users = "users"
-
     private val id = "id"
     private val host = "host"
     private val myEvents = "my-events"
@@ -28,60 +26,49 @@ class MyEventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_events)
-        auth = FirebaseAuth.getInstance()
-        database = FirebaseDatabase.getInstance().reference
         val myEventsListView = findViewById<ListView>(R.id.myEventsListView)
-        val currentUser = auth.currentUser
+        val currentUser = firebaseRepository.getCurrentUser()
         database.child(users).child(currentUser?.uid.toString()).child(myEvents).get().addOnSuccessListener {
             it.children.forEach {
                 myEventsID.add(
                         it.key.toString()
                 )
             }
-            Log.d("test", myEventsID.toString())
-            Log.i("firebase", "Got value ${it.value}")
+            for (id in myEventsID){
+                database.child(eventPath).child(id).get().addOnSuccessListener {
+                    myEventsList
+                        .add(
+                            Events(
+                                it.key.toString(),
+                                it.child("title").value.toString(),
+                                it.child("description").value.toString(),
+                                it.child("theme").value.toString(),
+                                it.child("date").value.toString(),
+                                it.child("latitude").value as Double,
+                                it.child("longitude").value as Double,
+                                it.child("host").value.toString(),
+                                it.child("guest-list").value as Map<User, String>
+                            )
+                        )
+                    myEventsListView.adapter = ArrayAdapter<Events>(
+                            this,
+                            android.R.layout.simple_list_item_1,
+                            android.R.id.text1,
+                            myEventsList
+                    )
+
+                    myEventsListView.setOnItemClickListener { parent, view, position, eventID ->
+                        val eventListItem = myEventsListView.adapter.getItem(position) as Events
+                        startActivity(Intent(this, UpdateMyEventActivity::class.java).putExtra("clickedId", eventListItem.eventID))
+                    }
+                }.addOnFailureListener{
+                    Log.e("firebase", "Error getting data", it)
+                }
+            }
+
         }.addOnFailureListener{
             Log.e("firebase", "Error getting data", it)
         }
-
-        for (id in myEventsID){
-            database.child(eventPath).child(id.toString()).get().addOnSuccessListener {
-                it.children.forEach {
-                    myEventsList.add(
-                        Events(
-                            it.key.toString(),
-                            it.child("title").value.toString(),
-                            it.child("description").value.toString(),
-                            it.child("theme").value.toString(),
-                            it.child("date").value.toString(),
-                                it.child("latitude").value as Double,
-                                it.child("longitude").value as Double,
-                            it.child("host").value.toString(),
-                            it.child("guest-list").value.toString()
-                        )
-                    )
-                }
-                Log.d("wäää", myEventsList.toString())
-                Log.d("test", myEventsID.toString())
-                Log.i("firebase", "Got value ${it.value}")
-            }.addOnFailureListener{
-                Log.e("firebase", "Error getting data", it)
-            }
-        }
-
-
-
-        /*myEventsListView.adapter = ArrayAdapter<EventID>(
-            this,
-            android.R.layout.simple_list_item_1,
-            android.R.id.text1,
-                myEventsID.toString()
-        )
-
-        myEventsListView.setOnItemClickListener { parent, view, position, id ->
-            val toDoItem = myEventsListView.adapter.getItem(position) as Events
-            startActivity(Intent(this, ToDoItemActivity::class.java).putExtra("clickedId", toDoItem.id))
-        }*/
 
     }
 }
