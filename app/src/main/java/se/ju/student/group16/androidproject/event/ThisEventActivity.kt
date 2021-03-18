a81da7f07344e84cd0bbe0b15e794abf7feeece1
Daniel Göthe <daniel@goethe.se>
2021-03-18 16:34:00 +0100

    this event activity added

---
+++ app/src/main/java/se/ju/student/group16/androidproject/event/ThisEventActivity.kt
@@ -0,0 +1,14 @@
+package se.ju.student.group16.androidproject.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import se.ju.student.group16.androidproject.R
+
+class ThisEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_this_event)
+
+
+    }
+}
