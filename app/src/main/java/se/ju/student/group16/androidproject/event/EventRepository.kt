047db80cb4dc491f215bb15a5fc07810a7a182f8
Filip Flodén <filipfloden@wlanguest33136141.eduroam.hj.se>
2021-03-18 17:27:50 +0100

    Update EventRepository.kt

--- app/src/main/java/se/ju/student/group16/androidproject/event/EventRepository.kt
+++ app/src/main/java/se/ju/student/group16/androidproject/event/EventRepository.kt
@@ -1,11 +1,16 @@
 package se.ju.student.group16.androidproject.event
 
+import android.util.Log
+import se.ju.student.group16.androidproject.firebaseRepository
+
 val eventRepository = EventRepository().apply{}
 
 class EventRepository {
     private val myEvents = mutableListOf<Events>()
     // upcomingEvents will contain both myEvents and the events the user is invited to
     private val upcomingEvents = mutableListOf<Events>()
+    private val database = firebaseRepository.getDatabaseReference()
+    private val currentUser = firebaseRepository.getCurrentUser()
 
     fun getAllMyEvents() = myEvents
     fun getAllUpcomingEvents() = upcomingEvents
@@ -18,20 +23,40 @@ class EventRepository {
                    eventDate: String,
                    eventLong: Double,
                    eventLat: Double,
-                   guestList: Map<String, String>){
-        myEvents.add(
-                Events(
-                        eventID,
-                        eventHost,
-                        eventTitle,
-                        eventDescription,
-                        eventTheme,
-                        eventDate,
-                        eventLong,
-                        eventLat,
-                        guestList
+                   guestList: Map<String, String>): Boolean {
+        if (myEvents.add(
+                        Events(
+                                eventID,
+                                eventHost,
+                                eventTitle,
+                                eventDescription,
+                                eventTheme,
+                                eventDate,
+                                eventLong,
+                                eventLat,
+                                guestList)
                 )
-        )
+        ){
+            Log.d("Added successfully", "brap")
+            return true
+        }else{
+            Log.d("Added failed", "brap")
+            return false
+        }
+        /*
+        val eventInfo = mapOf("host" to currentUser,"title" to eventTitle, "theme" to eventTheme,
+                "description" to eventDescription, "date" to eventDate, "latitude" to eventLat, "longitude" to eventLong)
+        Log.d("funkar","skicka till firebase")
+        val eventID = database.child("event").push().key
+        database.child("event").child(eventID.toString()).setValue(eventInfo)
+        for (invitedFriend in guestList) {
+            database.child("event").child(eventID.toString()).child("guest-list").child(invitedFriend.uid).setValue("pending")
+            database.child(users).child(invitedFriend.uid).child("upcoming-events").child(eventID.toString()).setValue(true)
+            database.child(users).child(currentUser).child("upcoming-events").child(eventID.toString()).setValue(true)
+            guestList[invitedFriend.uid] = "pending"
+        }
+        database.child(users).child(currentUser).child("my-events").child(eventID.toString()).setValue(true)
+        */
     }
     fun addUpcomingEvent(eventID: String,
                           eventHost: String,
@@ -86,6 +111,24 @@ class EventRepository {
                           newEventLat: Double,
                           newGuestList: Map<String, String>){
         getMyEventById(eventID)?.run {
+            eventTitle = newEventTitle
+            eventDescription = newEventDescription
+            eventTheme = newEventTheme
+            eventDate = newEventDate
+            eventLong = newEventLong
+            eventLat = newEventLat
+            guestList = newGuestList
+        }
+    }
+    fun updateUpcomingEvent(eventID: String,
+                          newEventTitle: String,
+                          newEventDescription: String,
+                          newEventTheme: String,
+                          newEventDate: String,
+                          newEventLong: Double,
+                          newEventLat: Double,
+                          newGuestList: Map<String, String>){
+        getUpcomingEventById(eventID)?.run {
             eventTitle = newEventTitle
             eventDescription = newEventDescription
             eventTheme = newEventTheme
